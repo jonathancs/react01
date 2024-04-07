37336d80a2c45e8296251d8ecb19cff46989f88c
casagrande <casagrandesoaresj@gmail.com>
2024-04-06 22:01:11 -0300

    n sei, mas funcionando.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,13 +3,6 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 
 export function Comment( {content, onDeleteComment} ) {
-    const [likeCount, setLikeCount] = useState(0);
-
-    function handleLikeComment() {
-        setLikeCount((state) => {
-            return state + 1
-        })
-    }
 
     function handleDeleteComment() {
 
@@ -18,7 +11,6 @@ export function Comment( {content, onDeleteComment} ) {
     return (
         <div className={styles.comment}>
             <Avatar src="https://github.com/jonathancs.png" alt="" />
-
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
@@ -26,24 +18,20 @@ export function Comment( {content, onDeleteComment} ) {
                             <strong>Jonathan Casagrande</strong>
                             <time title="11 de Maio às 08:13h" dateTime="2024-03-09 19:18:20">Cerca de 1h atrás</time>
                         </div>
-
                         <button onClick={handleDeleteComment}title='Deletar Comentário'>
                             <Trash size={20}/>
                         </button>
-
                     </header>
-
                     <p>{content}</p>
                 </div>
 
                 <footer>
-                    <button onClick={handleLikeComment}>
+                    <button>
                         <ThumbsUp/> 
-                        Aplaudir <span>{handleLikeComment}</span>
+                        Aplaudir <span>20</span>
                     </button>
                 </footer>
             </div>
         </div>
-
     )
 }
